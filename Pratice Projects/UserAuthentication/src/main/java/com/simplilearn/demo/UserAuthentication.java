1422473888c4838b201dcfc5a69c6548ca7dcd5f
makes <makeswari2001@gmail.com>
2022-08-26 23:13:38 +0530

    Phase 3

---
+++ Pratice Projects/UserAuthentication/src/main/java/com/simplilearn/demo/UserAuthentication.java
@@ -0,0 +1,15 @@
+package com.simplilearn.demo;
+
+public class UserAuthentication {
+	public String username()
+	{
+		String email = "abc@gmail.com";
+		return email;
+	}
+	
+	public String paswd()
+	{
+		String password = "1234";
+		return password;
+	}
+}
---
+++ Pratice Projects/UserAuthentication/src/test/java/com/simplilearn/demo/user.java
@@ -0,0 +1,56 @@
+package com.simplilearn.demo;
+
+import junit.framework.TestCase;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class user extends TestCase {
+	private UserAuthentication user;
+
+	@BeforeEach
+	public void setup() {
+		user = new UserAuthentication();
+		System.out.println("UserAuthentication class inititated");
+	}
+
+	@AfterEach
+	public void tearDown() {
+		user = null;
+		System.out.println("Class Closed");
+	}
+
+	@Test
+	public void checkUser() {
+		assertEquals("abc@gmail.com", user.username());
+	}
+
+	@Test
+	public void checkUserNull() {
+		assertNotNull(user.username());
+	}
+
+	@Test
+	public void checkPass() {
+		assertEquals("1234", user.paswd());
+	}
+
+	@Test
+	public void checkPassNull() {
+		assertNotNull(user.paswd());
+	}
+
+	@Test
+	public void checkUser1() {
+		String email = user.username();
+		String regex = "^(.+)@(.+)$";
+
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+
+		assertEquals(true, matcher.matches());
+
+	}
+}
